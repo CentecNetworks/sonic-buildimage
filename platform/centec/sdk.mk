735082aecba115b0dd22fcc2b14fb3f82d532f21
qianj <qianj@centec.com>
2025-06-26 05:48:37 +0000

    [MODIFY:CENTEC] update saiserver thrift to v2

--- platform/centec/sdk.mk
+++ platform/centec/sdk.mk
@@ -1,6 +1,13 @@
 # Centec SAI
-CENTEC_SAI = libsai_1.9.1-0_amd64.deb
-$(CENTEC_SAI)_URL = https://github.com/CentecNetworks/sonic-binaries/raw/master/amd64/sai/$(CENTEC_SAI)
-$(eval $(call add_conflict_package,$(CENTEC_SAI),$(LIBSAIVS_DEV)))
+export CENTEC_SAI_VERSION = 1.10.2-6
+export CENTEC_SAI = libsai_$(CENTEC_SAI_VERSION)_$(PLATFORM_ARCH).deb
+export CENTEC_SAI_DEV = libsai-dev_$(CENTEC_SAI_VERSION)_$(PLATFORM_ARCH).deb
+export CENTEC_SAI_URL_PREFIX = "https://github.com/CentecNetworks/sonic-binaries/raw/master/$(PLATFORM_ARCH)/sai"
+
+$(CENTEC_SAI)_URL = $(CENTEC_SAI_URL_PREFIX)/$(CENTEC_SAI)
+$(CENTEC_SAI_DEV)_URL = $(CENTEC_SAI_URL_PREFIX)/$(CENTEC_SAI_DEV)
+$(eval $(call add_conflict_package,$(CENTEC_SAI_DEV),$(LIBSAIVS_DEV)))
 
 SONIC_ONLINE_DEBS += $(CENTEC_SAI)
+SONIC_ONLINE_DEBS += $(CENTEC_SAI_DEV)
+$(CENTEC_SAI_DEV)_DEPENDS += $(CENTEC_SAI)
